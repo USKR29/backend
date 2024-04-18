1fd88ced1c4fd08a11b532e1f973cce8ed2aa1a5
USKR29 <uthayasankar29@gmail.com>
2024-04-18 14:32:45 +0530

    first commit

---
+++ src/api/catg/controllers/catg.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * catg controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::catg.catg');
---
+++ src/api/catg/routes/catg.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * catg router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::catg.catg');
